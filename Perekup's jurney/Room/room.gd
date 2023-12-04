ea024d65617908c323ded8d1557f9fe24c0c7dc6
BobGranata <murzataki@yandex.ru>
2023-12-04 21:13:31 +0500

    Добавлены сцены команты, фото на стене  и стола для работы. Добавил логику перехода по этим сценам из комнаты по клику на фотографии и столу. Новые сцены теперь загружаются через SceneSwitcher Добавлена сцена order в которой будет заказ покупателя.

---
+++ Perekup's jurney/Room/room.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+signal level_changed(level_name)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event.is_pressed() :	
+		emit_signal("level_changed", "Room/picture_on_the_wall")
+
+func _on_window_input_event(viewport, event, shape_idx):
+	if event.is_pressed() :
+		$Label.text = 'Window'
+
+
+func _on_area_2d_2_mouse_entered():
+	$Area2D2/AnimatedSprite2D.play()
+
+
+func _on_area_2d_2_mouse_exited():
+	$Area2D2/AnimatedSprite2D.stop()
+
+func _on_desk_input_event(viewport, event, shape_idx):
+	if event.is_pressed() :	
+		emit_signal("level_changed", "Room/desk")
+		
+func cleanup():	
+	queue_free()
---
+++ Perekup's jurney/SceneSwitcher.gd
@@ -0,0 +1,44 @@
+extends Node
+
+var next_level = null
+
+@onready var current_level = $Home
+@onready var anim = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	current_level.level_changed.connect(handle_level_changed)
+#	current_level.play_loaded_sound()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func handle_level_changed(current_level_name: String):
+	next_level = load("res://" + current_level_name + ".tscn").instantiate()
+	next_level.layer = -1
+	add_child(next_level)
+	anim.play("fade_in")
+
+	next_level.connect("level_changed", Callable(self, "handle_level_changed"))
+	
+	transfer_data_between_scenes(current_level, next_level)
+
+
+func transfer_data_between_scenes(old_scene, new_scene):
+#	new_scene.load_level_parameters(old_scene.level_parameters)
+	pass
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	match anim_name:
+		"fade_in":
+#			current_level.cleanup()
+			current_level.queue_free()
+			current_level = next_level
+			current_level.layer = 1
+			next_level = null
+			anim.play("fade_out")
+#		"fade_out":
+#			current_level.play_loaded_sound()
