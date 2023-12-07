6b375959e3190c7c1dc7f19ce0d13c9728ba0520
BobGranata <murzataki@yandex.ru>
2023-12-07 21:57:56 +0500

    Реализовал корректное выделение цветом, выбранного объявления. Id выбранного объявление теперь передаётся в сцену стола, для загрузки нужной машины в сцене покупки машины.

--- Perekup's jurney/Room/desk.gd
+++ Perekup's jurney/Room/desk.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 signal level_changed(level_name)
+signal reset_selection(id : int)
+
+var m_ad_id : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,10 +11,10 @@ func _ready():
 	var dict_car = BaseScript.m_dict_car
 	
 	for item_buy_car in dict_car:		
-		var ad = dict_car[item_buy_car]
-		var ad_ = dict_car[item_buy_car][BaseScript.PHONE]		
 		var new_car_ad = load("res://car_ad.tscn").instantiate()
-		new_car_ad.set_data(ad_)
+		new_car_ad.ad_selected.connect(set_ad_id)
+		new_car_ad.set_data(item_buy_car)
+		reset_selection.connect(new_car_ad.reset_item_color)
 		$ScrollContainer/VBoxContainer.add_child(new_car_ad)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,7 +28,11 @@ func _on_button_gui_input(event):
 func cleanup():	
 	queue_free()
 
+func set_ad_id(ad_id):
+	if ad_id != m_ad_id:
+		emit_signal("reset_selection", ad_id)
+	m_ad_id = ad_id	
 
 func _on_button_2_pressed():	
-	BaseScript.set_car_id(0)
+	BaseScript.set_car_id(m_ad_id)
 	emit_signal("level_changed", "BuyCar")
--- Perekup's jurney/car_ad.gd
+++ Perekup's jurney/car_ad.gd
@@ -1,5 +1,8 @@
 extends Control
 
+var m_ad_id : int
+
+signal ad_selected(id : int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,7 +13,9 @@ func _ready():
 func _process(delta):
 	pass
 
-func set_data(ad_model):
+func set_data(ad_id):
+	m_ad_id = ad_id
+	var ad_model = BaseScript.m_dict_car[m_ad_id][BaseScript.PHONE]	
 	$MarginContainer/VBoxContainer/Model.text = ad_model[BaseScript.MODEL]
 	$MarginContainer/VBoxContainer/Volume.text = ad_model[BaseScript.VOLUME]
 	pass
@@ -19,4 +24,8 @@ func set_data(ad_model):
 func _on_color_rect_gui_input(event):
 	if event.is_action_pressed("click") :
 		$ColorRect.color = "a7a7a7"
-	pass
+		emit_signal("ad_selected", m_ad_id)
+
+func reset_item_color(ad_id):
+	if m_ad_id != ad_id:
+		$ColorRect.color = "ffffff"
